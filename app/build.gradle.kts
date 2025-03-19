aa0d1e45e553059447551e755f23b37ddad45687
Thanh Dat Dev <thnhtbuitranc@gmail.com>
2025-03-19 14:37:26 +0700

    Update UI select seat&setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
 }
--- app/src/main/java/com/example/movieticketbox/ui/Profile/Profile.java
+++ app/src/main/java/com/example/movieticketbox/ui/Profile/Profile.java
@@ -1,38 +1,82 @@
+//package com.example.movieticketbox.ui.Profile;
+//
+//import androidx.lifecycle.ViewModelProvider;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.example.movieticketbox.R;
+//
+//public class Profile extends Fragment {
+//
+//    private ProfileViewModel mViewModel;
+//
+//    public static Profile newInstance() {
+//        return new Profile();
+//    }
+//
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_profile, container, false);
+//    }
+//
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+//        // TODO: Use the ViewModel
+//    }
+//
+//}
+
 package com.example.movieticketbox.ui.Profile;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.movieticketbox.R;
 
 public class Profile extends Fragment {
 
-    private ProfileViewModel mViewModel;
-
-    public static Profile newInstance() {
-        return new Profile();
-    }
-
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Gán sự kiện click đúng cách
+        view.findViewById(R.id.btnMyTicket).setOnClickListener(v -> openMyTickets());
+        view.findViewById(R.id.btnPaymentHistory).setOnClickListener(v -> openPaymentHistory());
+        view.findViewById(R.id.btnChangePassword).setOnClickListener(v -> openChangePassword());
+        view.findViewById(R.id.btnSelectSeat).setOnClickListener(v -> openSelectSeat());
+
+        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
-        // TODO: Use the ViewModel
+    private void openSelectSeat() {
+        startActivity(new Intent(getActivity(), SelectSeatActivity.class));
     }
 
+    private void openMyTickets() {
+        startActivity(new Intent(getActivity(), MyTicketActivity.class));
+    }
+
+    private void openPaymentHistory() {
+        startActivity(new Intent(getActivity(), PaymentHistoryActivity.class));
+    }
+
+    private void openChangePassword() {
+        startActivity(new Intent(getActivity(), ChangePasswordActivity.class));
+    }
 }
